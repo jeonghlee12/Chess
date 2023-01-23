b6e5948ddc9807566d68b700c2a9b006351c7ec5
jeonghlee12 <jl5987@columbia.edu>
2023-01-23 16:48:52 -0500

    Initial commit

---
+++ src/Board.java
@@ -0,0 +1,37 @@
+
+public class Board
+{
+	private char[][] board = new char[8][8];
+	
+	
+	public Board() {
+		
+	}
+	
+	
+	private void fen_to_board(String fen)
+	{
+		
+	}
+	
+	public void initializeBoard() {
+		board[0] = ['r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'];
+	}
+	
+	public char[][] getBoard() {
+		return board;
+	}
+	
+	public static void main(String[] args) {
+		Board b = new Board();
+		char[][] a = b.getBoard();
+		for (int i = 0; i < 8; i++)
+		{
+			for (int j = 0; j < 8; j++)
+			{
+				System.out.print(a[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
